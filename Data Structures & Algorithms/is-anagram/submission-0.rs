50491265f8e2bbfc2f9f3f03aac5f06ca9800a3e
pushpendu ∅ <pushpendupal441@gmail.com>
2026-07-26 19:08:39 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.rs
@@ -0,0 +1,26 @@
+impl Solution {
+    pub fn is_anagram(s: String, t: String) -> bool {
+        if s.len() != t.len(){
+            return false;
+        }
+
+        let mut map = HashMap::new();
+
+        for c in s.chars(){
+            let count = map.entry(c).or_insert(0);
+            *count +=1;
+
+        }
+
+        for c in t.chars(){
+            let count = map.entry(c).or_insert(0);
+            if *count == 0 {
+                return false;
+            }
+            *count -= 1;
+        }
+
+        true
+
+    }
+}
